0725c11d5b42a2edf2cf4fe1b62096dcb5e90a5b
Andrii <111363234+malandrii@users.noreply.github.com>
2024-12-01 01:40:04 -0500

    Project 4 starting file

--- Project 4/src/Main.java
+++ Project 4/src/Main.java
@@ -2,12 +2,7 @@
 // Passphrase Generator
 
 public class Main {
-    /**
-     * Main method to drive the application.
-     * @param args Command-line arguments.
-     */
     public static void main(String[] args) {
-        // Replace this with the actual path to the word list file
         PassphraseGenerator generator = new PassphraseGenerator("words.txt");
         generator.generatePassphrases();
     }
