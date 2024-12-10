6d71fdc2aa8263baaa089d322f46bb2ee539dad1
Andrii <111363234+malandrii@users.noreply.github.com>
2024-12-10 00:17:39 -0500

    Volunteer addition

---
+++ Project Final/src/Volunteer.java
@@ -0,0 +1,14 @@
+// Andrii Malakhovtsev
+// December 9, 2024
+// The Company
+
+public class Volunteer extends StaffMember {
+
+    public Volunteer(String name, String address, String phoneNumber) {
+        super(name, address, phoneNumber);
+    }
+
+    public double pay() {
+        return 0.0;
+    }
+}
